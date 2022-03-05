e4e13d7c7344d1c7d8e1f61a468daede99ae9988
hyeonsok <hyeonsok@student.42seoul.kr>
2022-03-06 04:15:46 +0900

    Rename directory cpp_module_00 -> module_00

--- cpp_module_00/ex01/eightact.cpp
+++
@@ -1,9 +0,0 @@
-#include "eightact.hpp"
-
-int		main(void)
-{
-	int	i;
-
-	
-	return (0);
-}
---
+++ module_00/ex01/eightact.cpp
@@ -0,0 +1,8 @@
+#include "eightact.hpp"
+
+int main(int argc, char *argv[])
+{
+	std::string input;
+
+	return (0);
+}
