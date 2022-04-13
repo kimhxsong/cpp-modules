1c08fe20c127889f9d590705a928871ae8287584
Hyeonsong Kim <kimhxsong@gmail.com>
2022-04-13 22:19:59 +0900

    cpp-module-05 ex00

---
+++ module_05/ex00/Bureaucrat.h
@@ -0,0 +1,45 @@
+#ifndef BUREAUCRAT_H_
+#define BUREAUCRAT_H_
+
+#include <exception>
+#include <ostream>
+#include <string>
+
+class Bureaucrat {
+ public:
+  class GradeTooHighException : public std::exception {
+   public:
+    // override
+    const char* what() const throw() { return "Exception: Grade Too High"; };
+  };
+
+  class GradeTooLowException : public std::exception {
+   public:
+    // override
+    const char* what() const throw() { return "Exception: Grade Too Low"; };
+  };
+
+  Bureaucrat();
+  Bureaucrat(const Bureaucrat& other);
+  Bureaucrat(const std::string& name, const unsigned int grade);
+
+  ~Bureaucrat();
+
+  Bureaucrat& operator=(const Bureaucrat& other);
+
+  const std::string& getName() const;
+  unsigned char getGrade() const;
+
+  void incrementGrade();
+  void decrementGrade();
+
+ private:
+  void setGrade(const unsigned int grade);
+
+  const std::string name_;
+  unsigned int grade_;
+};
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& rhs);
+
+#endif  // BUREAUCRAT_H_
