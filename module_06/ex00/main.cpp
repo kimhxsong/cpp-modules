7479450d8e760fada3440d5282bd5967922b6411
Hyeonsong Kim <kimhxsong@gmail.com>
2022-04-17 21:50:40 +0900

    ing

--- module_06/ex00/main.cpp
+++ module_06/ex00/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <iomanip>
 #include <sstream>
+#include <cstring>
+#include <string>
 
 // -inff, +inff, -inf, +inf, nan(not a number)
 // Non displayable, imposible 이런 문제들. 나는 적절한 유형인지만 평가하면됨.
@@ -44,18 +46,153 @@ int
 //   return 0;
 // } // float는 끝까지 읽음.
 // 
+enum etype {
+  TYPE_CH = 1,
+  TYPE_INT = 2,
+  TYPE_DOUBLE = 4,
+  TYPE_FLOAT = 8,
+  TYPE_INF,
+  TYPE_NAN
+};
+
+void validateArg(int argc, char *argv[]) {
+  if (argc == 2 || std::strlen(argv[1]) != 0) {
+    return;
+  }
+  throw std::strerror(EINVAL);
+}
 
 int main(int argc, char* argv[]) {
-  if (argc != 2) {
-    std::cout << "Error: It takes only one argument.\n";
-    return 1;
+
+  int type;
+  std::string istr(argv[1]);
+
+  try {
+    validateArg(argc, argv);
+  } catch (const std::string &s) {
+    std::cout << "validateArg(): " << s << '\n';
   }
-  int fnum;
-  std::stringstream sstrm(argv[1]);
-  sstrm >> fnum;
-  std::cout << "failbit" << sstrm.fail() << " eofbit " << sstrm.eof();
-  std::cout << " " << std::fixed << fnum << '\n';
-  sstrm >> fnum;
-  std::cout << "failbit" << sstrm.fail() << " eofbit " << sstrm.eof();
-  std::cout << " " << fnum << '\n';
+
+  try {
+    if (istr.length() == 1) {
+      if (std::isprint(istr[i]) == true) {
+        return TYPE_CH;
+      }
+      throw "invalid char literal";
+    }
+  } catch {
+    std::cout 
+  }
+    // throw exception()
+  std::cout << type << '\n';
+  // replaceDigitWithSpace
+  size_t  found;
+  if ((found = istr.find("nan", 0)) != std::string::npos) {
+    for (size_t i = found; i < found + 3; ++i) {
+      istr[i] = ' ';
+    }
+    istr[found] = '.';
+  }
+
+  if ((found = istr.find("inf", 0)) != std::string::npos) {
+    for (size_t i = found; i < found + 3; ++i) {
+      istr[i] = ' ';
+    }
+    istr[found] = '.';
+  }
+
+  for (size_t i = istr.length(); i-- > 0;) {
+    if (std::isdigit(istr[i]) || !std::isprint(istr[i])) {
+      istr[i] = ' ';
+    }
+  }
+
+  std::cout << "istr: " << istr << '\n';
+  std::istringstream iss(istr);
+  char ch;
+
+  // detectLiteralType()
+
+
+  // if (iss.eof()) {
+  //   return TYPE_INT;
+  // }
+
+  // if (ch == '.') {
+  //   iss >> std::skipws >> ch;
+  //   if (iss.eof()) {
+  //     return TYPE_DOUBLE;
+  // }
+  // }
+
+  // if (ch == 'f') {
+  //   iss >> std::skipws >> ch;
+  //   if (iss.eof()) {
+  //     return TYPE_FLOAT;
+  //   }
+  } 
+
+  char ch;
+  bool bDotFound = false;
+  
+  type = TYPE_INT;
+  iss >> std::skipws >> ch;
+  if (ch == '+' || ch == '-') {
+      iss >> std::skipws >> ch;
+  }
+
+  while (true)
+    iss >> std::skipws >> ch;
+    if (iss.eof()) break;
+
+    switch (ch) {
+      case '.':
+        bDotFound = true;
+        type = TYPE_DOUBLE;
+        break;
+      case 'f':
+        if (bDotFound == true) {
+          type = TYPE_FLOAT;
+          break;
+        }
+        // Intentional fall-through
+      default:
+        // throw() exception;
+        break;
+    }
+  }
+  return TYPE;
+
+
+  unsigned char marked = 00000;
+  std::cout << "ch: " << ch << '\n';
+  while (true) {
+    if (ch == '.' || !(iss >> std::skipws >> ch) && iss.eof()) break;
+
+    switch (ch) {
+      case '+':
+      case '-':
+        if (marked & 01000) return (-1);
+        marked |= 01000;
+        break;
+      case '.':
+        break;
+      default:
+        // throw() exception;
+        break;
+    }
+  }
+
+  if (iss.eof()) {
+    return type;
+  }
+
+  if (ch == 'f') {
+    iss >> std::skipws >> ch;
+    if (iss.eof()) {
+      type = TYPE_FLOAT;
+
+    } 
+  }
+  std::cout << type << '\n';
 }
